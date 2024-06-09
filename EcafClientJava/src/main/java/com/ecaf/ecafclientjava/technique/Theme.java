cfa71377746b96600b8e0a31b8e8ed5153d8250a
MohamedElTazi <102148964+MohamedElTazi@users.noreply.github.com>
2024-06-09 21:31:30 +0200

    Update Theme.java

--- EcafClientJava/src/main/java/com/ecaf/ecafclientjava/technique/Theme.java
+++ EcafClientJava/src/main/java/com/ecaf/ecafclientjava/technique/Theme.java
@@ -8,6 +8,7 @@ public class Theme{
     public static String themeAlert = "/com/ecaf/ecafclientjava/css/theme-clair/alert.css";
     public static String themeVueConnexion = "/com/ecaf/ecafclientjava/css/theme-clair/vueConnexion.css";
     public static String themeVueConnexionEchoueVide = "/com/ecaf/ecafclientjava/css/theme-clair/vueConnexionEchoueVide.css";
+    public static String themeVueCalendrier = "/com/ecaf/ecafclientjava/css/theme-clair/vueCalendrier.css";
     public static String backgroudColorMain = "#f0f0f0";
     public static String clair = "clair";
     public static String sombre = "sombre";
@@ -32,6 +33,7 @@ public class Theme{
         Theme.themeTableauFormulaire = "/com/ecaf/ecafclientjava/css/theme-clair/tableauFormulaire.css";
         Theme.themeAlert = "/com/ecaf/ecafclientjava/css/theme-clair/alert.css";
         Theme.themeVueConnexion = "/com/ecaf/ecafclientjava/css/theme-clair/vueConnexion.css";
+        Theme.themeVueConnexionEchoueVide = "/com/ecaf/ecafclientjava/css/theme-clair/vueConnexionEchoueVide.css";
         Theme.themeVueConnexionEchoueVide = "/com/ecaf/ecafclientjava/css/theme-clair/vueConnexionEchoueVide.css";
         updateSceneStyles(scene);
         updateVisibleElements(scene);
